ac7aeacd3f6dc6dbcfc2e99c35bf0490a8aa5cfd
Glory-CNBoy <68975045+Glory-CNBoy@users.noreply.github.com>
2023-02-03 13:21:09 +0800

    Update keepalive.sh

--- keepalive.sh
+++ keepalive.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # default values:
-# cpu: 12.5%
-# memory: 1/6(just work on arm instance)
-# network: 3M/s
+# cpu: 25%
+# memory: 1/3(just work on arm instance)
+# network: 10M/s
 
-durl="https://cdimage.debian.org/debian-cd/current/amd64/iso-dvd/debian-11.6.0-amd64-DVD-1.iso";
+durl="https://cdimage.ubuntu.com/ubuntustudio/releases/22.04.1/release/ubuntustudio-22.04.1-dvd-amd64.iso";
 
 ins_opt () {
     command -v apt &>/dev/null && ins='apt'
@@ -17,7 +17,7 @@ ins_opt () {
 }
 
 set_cpu_net () {
-    cpuq=$(lscpu | awk '/^CPU\(/{print $NF*100/8}');
+    cpuq=$(lscpu | awk '/^CPU\(/{print $NF*100/4}');
     [ -d "/opt/shuaibi" ] || mkdir -p /opt/shuaibi;
     cat << eof > /opt/shuaibi/cpu_net.sh;
     cpuc=$(lscpu | awk '/^CPU\(/{print $NF}');
@@ -29,14 +29,14 @@ set_cpu_net () {
     done
     while true;
     do
-        curl -skLo /dev/null "${durl}" --limit-rate 3M;
+        curl -skLo /dev/null "${durl}" --limit-rate 10M;
     done
     wait
 eof
 
     cat << eof > /lib/systemd/system/cpur.service
     [Unit]
-    Description=cpu stress 12.5 percents & download file with 3M/s speed
+    Description=cpu stress 25 percents & download file with 10M/s speed
     After=network.target
 
     [Service]
@@ -54,11 +54,11 @@ eof
 }
 
 set_mem () {
-    cat << eof > /opt/shuaibi/mem.sh;
+    cat << eof > /opt/shuaibi/mem.sh
     [ -d '/ramdisk' ] || mkdir -p /ramdisk;
     umount /ramdisk &>/dev/null;
     mem_count=\$(free -m|awk '/^Mem/{print \$2}');
-    ((mem_use=mem_count/6));
+    ((mem_use=mem_count/3));
     mount -t tmpfs -o size=\${mem_use}M tmpfs /ramdisk;
     img_size=\$(df -m /ramdisk|awk 'NR>1{print \$2-50}');
     dd if=/dev/zero of=/ramdisk/dd.img bs=1M count=\${img_size} &>/dev/null; 
