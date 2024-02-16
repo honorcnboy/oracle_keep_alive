8b1d4eba3877de716b6765d5b0568962dd4dc769
honorcnboy <68975045+honorcnboy@users.noreply.github.com>
2024-02-16 23:01:52 +0800

    Update keepalive.sh

--- Shuaibi/keepalive.sh
+++ Shuaibi/keepalive.sh
@@ -3,9 +3,9 @@
 # default values:
 # cpu: 25%
 # memory: 1/3(just work on arm instance)
-# network: 10M/s
+# network: 50M/s
 
-durl="https://cdimage.ubuntu.com/ubuntustudio/releases/22.04.1/release/ubuntustudio-22.04.1-dvd-amd64.iso";
+durl="https://cdimage.ubuntu.com/ubuntustudio/dvd/current/noble-dvd-amd64.iso";
 
 ins_opt () {
     command -v apt &>/dev/null && ins='apt'
@@ -29,14 +29,14 @@ set_cpu_net () {
     done
     while true;
     do
-        curl -skLo /dev/null "${durl}" --limit-rate 10M;
+        curl -skLo /dev/null "${durl}" --limit-rate 50M;
     done
     wait
 eof
 
     cat << eof > /lib/systemd/system/cpur.service
     [Unit]
-    Description=cpu stress 25 percents & download file with 10M/s speed
+    Description=cpu stress 25 percents & download file with 50M/s speed
     After=network.target
 
     [Service]
